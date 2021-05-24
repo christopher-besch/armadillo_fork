eea0f0a80226a85ab371899253bcdced9dbd2ff3
Christopher Besch <christopher.besch@gmx.de>
2021-05-24 11:02:49 +0200

    uncommented deps in config.hpp

---
+++ premake5.lua
@@ -0,0 +1,23 @@
+project "armadillo"
+    language "C"
+    architecture "x86_64"
+    cppdialect "C++17"
+    kind "StaticLib"
+
+    location ""
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+    filter ""
+
+    -- all files that are included or compiled = all files that are visible to the preprocessor/compiler/linker and make and stuff
+    files
+    {
+        "**.h",
+        "**.cpp"
+    }
+
+    -- for #include paths with ""
+    includedirs {
+        ""
+    }
