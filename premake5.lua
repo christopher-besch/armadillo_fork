b8891f0292e1497491decb472a962116574bee31
Christopher Besch <christopher.besch@gmx.de>
2021-05-24 12:19:12 +0200

    updated premake

--- premake5.lua
+++ premake5.lua
@@ -1,5 +1,5 @@
 project "armadillo"
-    language "C"
+    language "C++"
     architecture "x86_64"
     cppdialect "C++17"
     kind "StaticLib"
@@ -8,16 +8,14 @@ project "armadillo"
     targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
     objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
 
-    filter ""
-
     -- all files that are included or compiled = all files that are visible to the preprocessor/compiler/linker and make and stuff
     files
     {
-        "**.h",
-        "**.cpp"
+        "include/**.hpp",
+        "src/**.cpp"
     }
 
     -- for #include paths with ""
     includedirs {
-        ""
+        "include"
     }
